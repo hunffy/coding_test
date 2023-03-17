eac1ec93daea70d02c3db568bb29e54b6c48c200
sehun <sehun910124@naver.com>
2023-03-17 12:25:26 +0900

    코딩테스트 문제

---
+++ coding_test/23_03_17_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 12:00:27 2023
+
+03_17_test
+"""
+#########입출력과 사칙연산###############
+
+# 1. hello world 출력
+print("Hello World")
+
+
+# 2. 고양이 출력하기
+print("\\    /\\")
+print(" )  ( ')"   )
+print("(  /  )")
+print(" \\(__)|")
+
+# 3. 두 정수 A와 B를 입력받아 A+B를 출력하는 프로그램 만들기.
+
+A = int(input("A값을 입력해주세요."))
+B = int(input("B값을 입력해주세요."))
+
+print(A+B)
+
