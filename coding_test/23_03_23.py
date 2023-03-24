1082cca43fa62f1c35d1720d872b8a9fdfcb2bd7
sehun <sehun910124@naver.com>
2023-03-24 15:14:40 +0900

    슬라이싱예제 수정

--- coding_test/23_03_23.py
+++ coding_test/23_03_23.py
@@ -72,42 +72,5 @@ if cnt_list.count(max(cnt_list))>1 :
 else :
     max_index = cnt_list.index(max(cnt_list))
     print(unique[max_index])
-    
-    
-# 4. 바구니 순서 바꾸기
 
-# N, M = map(int,input("바구니갯수N과 셔플횟수M을 입력하세요").split())
-N = 10
-M = 5
-N_list = []
-for i in range(1,N+1) :
-    N_list.append(i)
     
-for i in range(1, M+1) : 
-    i,j,k = list(map(int, input("i,j,k입력: ").split())) #1,6,4   
-    print(N_list)
-    temp_list = N_list[i-1:j]
-    print(temp_list)
-    print(temp_list[k-1:j] + N_list[j+1:])
-    # N_list[i-1:j] = temp_list[k-1:j] + temp_list[i-1:k-1]
-    print(N_list)
-    print()
-    
-    
-    
-#4-1
-
-N, M = map(int,input("N 과 M을 입력하세요.").split())
-
-N_list = []
-for i in range(1,N+1) :
-    N_list.append(i)
-    
-for i in range(1,M+1) :
-    i,j,k = list(map(int, input("i,j,k는?").split())) # i:1 , j:6, k:4
-    print(N_list)
-    print(N_list[:i-1]+N_list[k-1:j]+N_list[i-1:k-1]+N_list[j:])    
-    N_list = N_list[:i-1]+N_list[k-1:j]+N_list[i-1:k-1]+N_list[j:]   
-    
-
-print(N_list[:i-1])
