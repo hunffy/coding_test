4bbf00f8f0670a4d524fa1e0eb5c9689cc95cdff
sehun <sehun910124@naver.com>
2023-04-03 15:19:37 +0900

    2차원 배열덧셈 최종수정

--- coding_test/test_0403/test_0403_2.java
+++ coding_test/test_0403/test_0403_2.java
@@ -10,36 +10,36 @@ import java.util.Scanner;
 
 public class test_0403_2 {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("N과 M을 입력해주세요.");
-        int N = scan.nextInt();
-        int M = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.print("N과 M값을 입력해주세요.");
+    int N = scan.nextInt();
+    int M = scan.nextInt();
+    int [][] arr1 = new int[N][M];
+    int [][] arr2 = new int[N][M];
+    
+    //arr1 배열 값 입력
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            System.out.print("arr1 값을 입력해주세요.");
+            arr1[i][j]=scan.nextInt();
+        }
+    }
+    //arr2 배열 값 입력
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            System.out.print("arr2 값을 입력해주세요.");
+            arr2[i][j] = scan.nextInt();
+        }
+    }
 
-        int [][] arr1 = new int[N][M];
-        int [][] arr2 = new int[N][M];
-        
-        //arr1배열 각 요소들 입력
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                System.out.println("arr1값을 입력해주세요.");
-                arr1[i][j]=scan.nextInt();
+    //arr1+arr2 배열의 합 결과 도출
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            System.out.print(arr1[i][j]+arr2[i][j]+" ");
+            if( j == M-1){
+                System.out.println();
             }
-        }
-        //arr2배열 각 요소들 입력
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                System.out.println("arr2값을 입력해주세요.");
-                arr2[i][j]=scan.nextInt();
-            }
-        }
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                System.out.print("결과:"+arr1[i][j]+arr2[i][j]+" ");
-                if(j == M-1){
-                    System.out.println();
-                }
-            }
-        }
+        }  
+    }
     }
 }
