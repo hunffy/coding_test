441effd11fe3a61e4c885b7c0318e73cf0b9135f
sehun <sehun910124@naver.com>
2023-04-03 16:13:53 +0900

    2차원배열 최댓값, 인덱스값구하기 예제

---
+++ coding_test/test_0403/test_0403_3.java
@@ -0,0 +1,27 @@
+package test_0403;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class test_0403_3 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int [][] arr = new int[9][9];
+        int max = 0;
+        int x =0;
+        int y =0;
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print("요소의 값을 입력하세요.");
+                arr[i][j] = in.nextInt();
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                    x=i; y=j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(x+"행"+y+"열");
+    }
+}
