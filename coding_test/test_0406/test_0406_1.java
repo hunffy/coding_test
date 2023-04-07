2186fcab453577f393e396ad80c019a88b56a8a3
sehun <sehun910124@naver.com>
2023-04-07 11:43:43 +0900

    진법변환 예제 주석추가

--- coding_test/test_0406/test_0406_1.java
+++ coding_test/test_0406/test_0406_1.java
@@ -12,8 +12,11 @@ public class test_0406_1 {
         System.out.println("진법 B를 입력해주세요.");
         int B = scan.nextInt();
         for(int i=0; i<N.length(); i++) {
+            //입력값이 0~9사이일때
+            //N.charAt(i)는 문자이기때문에 '0'~'9'일때는 '0'을뺴줘서 숫자로표현, 출력해준다.
             if('0' <= N.charAt(i) && '9'>=N.charAt(i))
                 ans = ans*B+(N.charAt(i)-'0');
+            //입력값이 A~Z일때 문자 'Z'의 값을 숫자료 표현하기위해 문자'A'를 빼주고 10을 더해준다.
             else
                 ans = ans*B+(N.charAt(i)-'A'+10);
         }
