64f6d71c8e648a1e094782db3b40a056bbf3449c
SehunKim <109889070+hunffy@users.noreply.github.com>
2023-09-26 16:57:18 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,10 +1,9 @@
 function solution(num) {
     var answer = '';
     if(num%2==0){
-        answer='Even'
+        answer = "Even";
     }else{
-        answer='Odd'
+        answer = "Odd";
     }
     return answer;
 }
-
