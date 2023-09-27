7503052b79f28d9568eba377c35a6301bbd7f767
SehunKim <109889070+hunffy@users.noreply.github.com>
2023-09-27 10:07:59 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,11 @@
 function solution(n)
 {
     var answer = 0;
-    while(n>0){
-        answer +=n%10;
-        n=Math.floor(n/10);
+    //자연수 n을 문자열로 변환 num = "123"
+    var num = n.toString();
+    //반복문을 통해 쪼갠 후 parseInt 정수형으로 바꿔서 더한값을 저장해준다.
+    for(let i=0; i<num.length; i++){
+        answer += parseInt(num[i]);
     }
     return answer;
 }
