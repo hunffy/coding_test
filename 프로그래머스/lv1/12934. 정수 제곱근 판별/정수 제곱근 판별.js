2a613a6eb82bb525a1019ce4a57aae73cb33e384
SehunKim <109889070+hunffy@users.noreply.github.com>
2023-09-27 10:31:57 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,18 @@
 function solution(n) {
     var answer = 0;
     
-    if(Math.sqrt(n)%1!=0){
-        answer=-1;
+    //제곱근을 구하는 함수. n이 121이라면 sqrt는 11
+    var sqrt = Math.sqrt(n);
+    
+    //제곱근 중 소수가있을수 있기때문에 위에서 구한 제곱근을 정수로 변환
+    var res = Math.floor(sqrt);
+    
+    //제곱근을 확인하는 구간
+    if (res * res == n){
+        answer = (res+1)*(res+1);
+    //제곱근이 아니라면 -1리턴.
     }else{
-        answer = (Math.sqrt(n)+1)*(Math.sqrt(n)+1);
+        answer = -1;
     }
     return answer;
 }
