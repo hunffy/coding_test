6ed0e95d221476a7a61ae45bce774cafc70aa797
SehunKim <109889070+hunffy@users.noreply.github.com>
2023-09-27 13:51:56 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,16 +1,10 @@
 function solution(arr) {
     var answer = [];
-    for(let i=0; i<arr.length; i++){
-        if(arr.length==1){
-            answer[i]=-1;
-        }else{
-            let min = Math.min(...arr);
-            if(arr[i]===min){
-                continue;
-            }else{
-                answer.push(arr[i]);
-            }
-        }
+    var min = Math.min(...arr);
+    answer = arr.filter(num => num!==min);
+    
+    if(answer.length===0){
+        return [-1];
     }
     return answer;
 }
