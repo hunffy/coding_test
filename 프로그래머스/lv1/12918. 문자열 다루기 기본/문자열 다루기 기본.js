8609c9efe28990ba9120e8c03cd1e7b628a3fc81
SehunKim <109889070+hunffy@users.noreply.github.com>
2023-09-28 13:08:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,13 +1,18 @@
 function solution(s) {
     var answer = true;
-    if(s.length ==4 || s.length ==6){
+    if(s.length==4 || s.length==6){
         for(let i=0; i<s.length; i++){
-            if(!('0'<=s.charAt(i) && s.charAt(i)<='9')){
+            if('a'<=s.charAt(i) && s.charAt(i)<='z'){
                 answer = false;
                 break;
+            }else if('A'<=s.charAt(i) && s.charAt(i)<='Z'){
+                answer = false;
+                break;
+            }else {
+                answer = true;
             }
         }
-    }else {
+    }else{
         answer = false;
     }
     return answer;
